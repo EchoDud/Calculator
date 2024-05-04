e92d1a2f333f9f06901b43a276f8b64835fab018
Слава Дудин <ehot_dudin@list.ru>
2024-05-04 09:23:10 +0500

    Красивый интерфейс

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,12 +5,13 @@ namespace Calculator
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddControllersWithViews(); // Enable MVC
+            builder.Services.AddControllersWithViews();
             builder.Services.AddSingleton<MongoDBService>();
 
             var app = builder.Build();
             app.UseStaticFiles();
             app.UseRouting();
+            app.UseStaticFiles();
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
