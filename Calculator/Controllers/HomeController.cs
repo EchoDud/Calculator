a31488771eebd036c57089439fae61602df172b0
Слава Дудин <ehot_dudin@list.ru>
2024-05-04 09:03:56 +0500

    Добавил степень

--- Calculator/Controllers/HomeController.cs
+++ Calculator/Controllers/HomeController.cs
@@ -60,24 +60,33 @@ namespace Calculator.Controllers
                 switch (name.ToLower())
                 {
                     case "sin":
-                        args.Result = Math.Sin(Convert.ToDouble(args.Parameters[0].Evaluate()));
+                        args.Result = Math.Sin(Convert.ToDouble(args.Parameters[0].Evaluate(), CultureInfo.InvariantCulture));
                         break;
                     case "cos":
-                        args.Result = Math.Cos(Convert.ToDouble(args.Parameters[0].Evaluate()));
+                        args.Result = Math.Cos(Convert.ToDouble(args.Parameters[0].Evaluate(), CultureInfo.InvariantCulture));
                         break;
                     case "tan":
-                        args.Result = Math.Tan(Convert.ToDouble(args.Parameters[0].Evaluate()));
+                        args.Result = Math.Tan(Convert.ToDouble(args.Parameters[0].Evaluate(), CultureInfo.InvariantCulture));
                         break;
                     case "log":
-                        args.Result = Math.Log10(Convert.ToDouble(args.Parameters[0].Evaluate()));
+                        args.Result = Math.Log10(Convert.ToDouble(args.Parameters[0].Evaluate(), CultureInfo.InvariantCulture));
                         break;
                     case "ln":
-                        args.Result = Math.Log(Convert.ToDouble(args.Parameters[0].Evaluate()));
+                        args.Result = Math.Log(Convert.ToDouble(args.Parameters[0].Evaluate(), CultureInfo.InvariantCulture));
+                        break;
+                    case "pow":
+                        if (args.Parameters.Length == 2)
+                        {
+                            double baseValue = Convert.ToDouble(args.Parameters[0].Evaluate(), CultureInfo.InvariantCulture);
+                            double exponent = Convert.ToDouble(args.Parameters[1].Evaluate(), CultureInfo.InvariantCulture);
+                            args.Result = Math.Pow(baseValue, exponent);
+                        }
                         break;
                 }
             };
             return Convert.ToDouble(evaluator.Evaluate());
         }
-        
+
     }
+
 }
